6632871d433bc88a183a1fe301c03c4679be5b4c
Luis Vu <luisvu.work@gmail.com>
2026-07-10 17:17:14 +0700

    Enhance test_verify_dashboard_overview with allure
    Add allure steps and title to dashboard overview test

--- tests/ui/dashboard/test_dashboard_overview.py
+++ tests/ui/dashboard/test_dashboard_overview.py
@@ -1,4 +1,9 @@
+import allure
+@allure.title("Luis test title")
 def test_verify_dashboard_overview(dashboard_page):
+    with allure.step("Step1")
     assert dashboard_page.is_menu_visible(), "Left menu is not displayed"
+    with allure.step("Step2")
     assert dashboard_page.is_widget_visible(), "Widget Dashboard is not displayed"
+    with allure.step("Step3")
     dashboard_page.search_name("Anh Tester")
